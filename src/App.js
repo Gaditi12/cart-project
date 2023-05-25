8dd2f2644451d56fdf096dbd458227e3619eedcf
aditigupta <aditi.gupta@deeporion.com>
2023-05-25 16:24:57 +0530

    completed UI part

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "./components/Header";
+import { Routes, Route } from "react-router-dom";
+import CardsDetails from "./components/CardsDetails";
+import Cards from "./components/Cards";
 export default function App() {
   return (
     <>
       <Header />
+      <Routes>
+        <Route path="/" element={<Cards />} />
+        <Route path="/cartdetail" element={<CardsDetails />} />
+      </Routes>
     </>
   );
 }
